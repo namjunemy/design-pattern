56f346bd8474640296efd746ac874209fba7e373
Namjun Kim <namjunemy@gmail.com>
2018-04-17 02:44:04 +0900

    factory method pattern ex code

---
+++ 04_factory_method_pattern/src/concrete/Main.java
@@ -0,0 +1,19 @@
+package concrete;
+
+import framework.Item;
+import framework.ItemCreator;
+
+public class Main {
+  public static void main(String[] args) {
+    ItemCreator itemCreator;
+    Item item;
+
+    itemCreator = new HpCreator();
+    item = itemCreator.create();
+    item.use();
+
+    itemCreator = new MpCreator();
+    item = itemCreator.create();
+    item.use();
+  }
+}
---
+++ 04_factory_method_pattern/src/framework/ItemCreator.java
@@ -0,0 +1,15 @@
+package framework;
+
+public abstract class ItemCreator {
+  public Item create() {
+    requestItemsInfo();
+    Item item = createItem();
+    createItemLog();
+
+    return item;
+  }
+
+  abstract protected void requestItemsInfo();
+  abstract protected void createItemLog();
+  abstract protected Item createItem();
+}
